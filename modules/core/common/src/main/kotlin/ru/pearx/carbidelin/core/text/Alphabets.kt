ef2b0f713910d8edb240e28855b884b9c7bd1448
mrAppleXZ <mr.applexz@gmail.com>
2019-03-29 18:04:20 +0500

    Updated the copyright notice

--- modules/core/common/src/main/kotlin/ru/pearx/carbidelin/core/text/Alphabets.kt
+++ modules/core/common/src/main/kotlin/ru/pearx/carbidelin/core/text/Alphabets.kt
@@ -1,8 +1,8 @@
 /*
- *  Copyright © 2018 mrAppleXZ
- *  This Source Code Form is subject to the terms of the Mozilla Public
- *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
- *  You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright © 2019, PearX Team
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 package ru.pearx.carbidelin.core.text
--- modules/uri/build.gradle.kts
+++ modules/uri/build.gradle.kts
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2019, PearX Team
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import ru.pearx.multigradle.util.*
 import ru.pearx.multigradle.util.Platform
 import ru.pearx.multigradle.util.js.MultiGradleJsExtension
