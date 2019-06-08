aca90278757a4cabc936f0e635179d4d30bed804
mrAppleXZ <mr.applexz@gmail.com>
2019-06-08 12:53:50 +0500

    Added Float and Double isWhole() checks

--- modules/math/src/commonMain/kotlin/ru/pearx/carbidelin/math/NumberHelper.kt
+++ modules/math/src/commonMain/kotlin/ru/pearx/carbidelin/math/NumberHelper.kt
@@ -2,3 +2,5 @@ package ru.pearx.carbidelin.math
 
 
 fun Int.cycle(min: Int, maxInclusive: Int) = ((this - min).rem(maxInclusive - min)) + min
+fun Float.isWhole() = this != this.toInt().toFloat()
+fun Double.isWhole() = this != this.toInt().toDouble()
